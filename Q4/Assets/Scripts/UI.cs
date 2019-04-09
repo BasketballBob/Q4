8e6268500172d13f25d62228613a9a355c8f8422
Legokid1 <38771068+Legokid1@users.noreply.github.com>
2019-04-09 13:28:34 -0700

    Gui work

--- Q4/Assets/Scripts/UI.cs
+++ Q4/Assets/Scripts/UI.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class UI : MonoBehaviour
 {
-    public int x, y, w, h;
+    public int cx, cy, cw, ch;
+    public int hx, hy, hw, hh;
+    public int icx, icy;
+    public int ihx, ihy;
     public Font font;
     public int fontSize;
-    public Texture2D textureToDisplay;
+    public static int currency;
+    public static int health;
+    public Texture2D CurencyTexture,HealthTexture;
+    
     private void OnGUI()
     {
         GUI.skin.label.font = GUI.skin.button.font = GUI.skin.box.font = font;
         GUI.skin.label.fontSize = GUI.skin.box.fontSize = GUI.skin.button.fontSize = fontSize;
 
-        //Displays Text at the given corintaes
-        GUI.Label(new Rect(x, y, w, h), "Hello World!");
+        //Displays Curency at the given corintaes
+        GUI.Label(new Rect(cx, cy, cw, ch), currency.ToString());
 
-        //Displays a Texture at the given corintaes and sets it at the given texeture width and hight
-        GUI.DrawTexture(new Rect(x, y, textureToDisplay.width, textureToDisplay.height), textureToDisplay);
+        //Displays Health at the given corinates
+        GUI.Label(new Rect(hx, hy, hw, hh), health.ToString());
+
+        //Displays the currecy texture at given cordinates
+        GUI.DrawTexture(new Rect(icx, icy, CurencyTexture.width, CurencyTexture.height), CurencyTexture);
+        //Displays the health texture at given cordinates
+        GUI.DrawTexture(new Rect(ihx, ihy, HealthTexture.width, HealthTexture.height), HealthTexture);
+
 
 
     }
